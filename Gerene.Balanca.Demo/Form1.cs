29700db39600afa0c9a20c6f3dc07b91d78211ab
Marcos Gerene Felix Spirito <mgf.spirito@gmail.com>
2021-01-29 11:27:54 -0300

    Add project files.

---
+++ Gerene.Balanca.Demo/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gerene.Balanca.Demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.DataSource = Balanca.ListarPortas();
+            comboBox2.DataSource = Enum.GetValues(typeof(ModeloBalanca));
+        }
+
+        private Balanca _Balanca;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Desconecta antes
+            if (_Balanca != null)
+            {
+                if (_Balanca.IsConectada)
+                    _Balanca.Dispose();
+
+                _Balanca = null;
+            }
+
+            _Balanca = new Balanca()
+            {
+                NomePorta = comboBox1.Text,
+                Modelo = (ModeloBalanca)Convert.ToInt32(comboBox2.SelectedValue),
+                BaudRate = (int)numericUpDown1.Value,
+                Timeout = (int)numericUpDown2.Value,
+                IsMonitorar = checkBox1.Checked,
+                DelayMonitoramento = (int)numericUpDown3.Value
+            };
+
+            _Balanca.AoLerPeso += _Balanca_AoLerPeso;
+            _Balanca.AoLancarExcessao += _Balanca_AoLancarExcessao;
+
+            _Balanca.Conectar();
+        }        
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (_Balanca != null)
+            {
+                if (_Balanca.IsConectada)
+                    _Balanca.Dispose();
+
+                _Balanca = null;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (_Balanca == null || !_Balanca.IsConectada)
+            {
+                MessageBox.Show("Balança não conectada");
+                return;
+            }
+
+            _Balanca.LerPeso();
+
+        }
+
+        private void _Balanca_AoLerPeso(object sender, Balanca.BalancaEventArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                label7.Text = $"Ultimo peso {e.Peso:N3} Kg";
+
+                textBox1.Text += $"{DateTime.Now:dd/MM/yyyy HH:mm:ss} - {e.Peso:N3} Kg" + Environment.NewLine;
+            });
+
+            Application.DoEvents();
+        }
+
+        private void _Balanca_AoLancarExcessao(object sender, Balanca.BalancaEventArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                textBox2.Text += $"{DateTime.Now:dd/MM/yyyy HH:mm:ss} - {e.Excessao.Message}" + Environment.NewLine;
+            });
+
+            Application.DoEvents();
+        }
+        
+
+    }
+}
---
+++ Gerene.Balanca/Balanca.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Gerene.Balanca
+{
+    /// <summary>
+    /// Modelo 1 - Protocolo Toledo e similares
+    /// Modelo 2 - Protocolo Filizola e similares
+    /// </summary>
+    public enum ModeloBalanca
+    {
+        Modelo1,
+        Modelo2
+    }
+
+    public class Balanca : IDisposable
+    {
+        #region Atributos
+        private SerialPort _Serial { get; set; }
+        public ModeloBalanca Modelo { get; set; }
+        public string NomePorta { get; set; }
+        public int BaudRate { get; set; }
+        public int Timeout { get; set; }
+        private bool _IsMonitorar;
+        public bool IsMonitorar
+        {
+            get => _IsMonitorar;
+            set
+            {
+                _IsMonitorar = value;
+            }
+        }
+        public int DelayMonitoramento { get; set; }
+
+        public DateTime UltimaLeitura { get; private set; }
+        public decimal UltimoPeso { get; private set; }
+        #endregion
+
+        #region Construtor
+        public Balanca()
+        {
+            _Serial = new SerialPort();
+            Modelo = ModeloBalanca.Modelo1;
+            NomePorta = "COM1";
+            BaudRate = 9600;
+            Timeout = 300;
+
+            _IsMonitorar = false;
+            DelayMonitoramento = 1000;
+        }
+
+        ~Balanca() => Dispose();
+
+        public void Dispose()
+        {
+            //Para o monitoramento
+            if (_Cancelamento != null)
+                _Cancelamento.Cancel();
+
+            //Libera a porta serial
+            if (_Serial != null)
+            {
+                _Serial.Dispose();
+                _Serial = null;
+            }
+        }
+        #endregion
+
+        #region Eventos
+        public class BalancaEventArgs : EventArgs
+        {
+            public decimal Peso { get; private set; }
+            public Exception Excessao { get; set; }
+
+            public BalancaEventArgs(decimal peso)
+            {
+                Peso = peso;
+            }
+
+            public BalancaEventArgs(Exception exception)
+            {
+                Excessao = exception;
+            }
+        }
+
+        public event EventHandler<BalancaEventArgs> AoLerPeso;
+        public event EventHandler<BalancaEventArgs> AoLancarExcessao;
+        #endregion
+
+        #region Métodos
+        public static string[] ListarPortas() => SerialPort.GetPortNames();
+
+        public void Conectar()
+        {
+            if (_Serial.IsOpen)
+                throw new ArgumentException("A porta já está aberta");
+
+            _Serial.PortName = NomePorta;
+            _Serial.BaudRate = BaudRate;
+            _Serial.ReadTimeout = Timeout;
+            _Serial.Open();
+
+            if (_IsMonitorar)
+            {
+                _Cancelamento = new CancellationTokenSource();
+                Monitorar();
+            }
+        }
+
+        public decimal LerPeso()
+        {
+            decimal pesolido = 0;
+
+            try
+            {
+                if (_Serial == null || !_Serial.IsOpen)
+                    throw new ArgumentException("A porta serial não está aberta");
+
+                string dados = _Serial.ReadExisting();
+
+                if (string.IsNullOrEmpty(dados))
+                    return 0;
+
+                switch (Modelo)
+                {
+                    case ModeloBalanca.Modelo1:
+                        dados = dados.Substring(dados.Length - 6, 5);
+                        break;
+                    case ModeloBalanca.Modelo2:
+                        dados = dados.Substring(dados.Length - 5);                        
+                        break;
+                }
+
+                pesolido = decimal.Parse(dados) / 1000M;
+
+                UltimoPeso = pesolido;
+                UltimaLeitura = DateTime.Now;
+
+                if (AoLerPeso != null)
+                    AoLerPeso.Invoke(this, new BalancaEventArgs(pesolido));
+
+                return pesolido;
+            }
+            catch (Exception ex)
+            {
+                if (AoLancarExcessao != null)
+                    AoLancarExcessao.Invoke(this, new BalancaEventArgs(ex));
+
+                throw;
+            }
+        }
+
+        private CancellationTokenSource _Cancelamento;
+
+        private async void Monitorar()
+        {
+            await Task.Run(async () =>
+            {
+                while (_IsMonitorar && !_Cancelamento.Token.IsCancellationRequested)
+                {
+                    try
+                    {
+                        LerPeso();
+                    }
+                    catch
+                    {
+                        //Não preciso fazer tratamentos aqui, o próprio ler peso chama o evento "AoLancarExcessao" para o usuário tratar
+                    }
+                    finally
+                    {
+                        await Task.Delay(DelayMonitoramento);
+                    }
+                }
+            }, _Cancelamento.Token);
+        }
+
+        public bool IsConectada => _Serial != null && _Serial.IsOpen;
+        #endregion
+
+    }
+}
